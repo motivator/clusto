29e2ea14e2be138edf57f2cba58668ccef1944dd
Mike Newton <mike@delusion.org>
2012-06-07 11:21:08 -0700

    this may be a weird/lame? way to solve ExclusivePool being really exclusive, but...

--- src/clusto/drivers/categories/pool.py
+++ src/clusto/drivers/categories/pool.py
@@ -5,66 +5,9 @@ from clusto.exceptions import PoolException
 
 from itertools import imap, chain
 
-class Pool(Driver):
-    """
-    A Pool is used to group Entities into a collection that shares attributes.
-
-    Pools
-    """
-
-    _driver_name = "pool"
-    _clusto_type = "pool"
-
-
-    def insert(self, thing):
-        """Insert the given Enity or Driver into this Entity.
-
-        Such that:
-
-        >>> A.insert(B)
-        >>> (B in A)
-        True
-
-        A given entity can only be in a Pool one time.
-        """
-
-        d = self.ensure_driver(thing,
-                               "Can only insert an Entity or a Driver. "
-                               "Tried to insert %s." % str(type(thing)))
-
-        if d in self:
-            raise PoolException("%s is already in pool %s." % (d, self))
-
-        self.add_attr("_contains", d, number=True)
-
-
-    def is_parent(self, thing):
-        """
-        Is this pool the parent of the given entity
-        """
-
-        d = self.ensure_driver(thing,
-                               "Can only be the parent of a Driver or Entity.")
-
-        return self in d.contents()
-
-    @classmethod
-    def get_pools(cls, obj, allPools=True):
-
-        d = cls.ensure_driver(obj, "obj must be either an Entity or a Driver.")
-
-
-        pools = [Driver(a.entity) for a in d.parents()
-                 if isinstance(Driver(a.entity), Pool)]
-
-        if allPools:
-            for i in pools:
-                pools.extend(Pool.get_pools(i, allPools=True))
-
-        return pools
-
-class ExclusivePool(Pool):
+class ExclusivePool(Driver):
     _driver_name = "exclusive_pool"
+    _clusto_type = "pool"
 
     def insert(self, thing):
         """Insert the given Enity or Driver into this Entity.
@@ -79,14 +22,75 @@ class ExclusivePool(Pool):
         it is in NO other pools.
         """
 
-        pools = Pool.get_pools(thing)
-        if pools:
-            raise PoolException("%s is already in pools %s, cannot insert "
-                                "exclusively." % (thing, pools))
+        d = self.ensure_driver(thing,
+                               "Can only insert an Entity or a Driver. "
+                               "Tried to insert %s." % str(type(thing)))
 
-        Pool.insert(self, thing)
+        pools = self.get_pools(thing)
+        if self._driver_name = 'exclusive_pool':
+            if pools:
+                raise PoolException("%s is already in pools %s, cannot insert "
+                                    "exclusively." % (thing, pools))
+        else:
+            for i in pools:
+                if i._driver_name = 'exclusive_pool':
+                    raise PoolException("%s is in ExclusivePool %s, cannot insert "
+                                        "into any other pool." % (thing, i))
 
-class UniquePool(Pool):
+        self.add_attr("_contains", d, number=True)
+
+    def is_parent(self, thing):
+        """
+        Is this pool the parent of the given entity
+        """
+
+        d = self.ensure_driver(thing,
+                               "Can only be the parent of a Driver or Entity.")
+
+        return self in d.contents()
+
+    @classmethod
+    def get_pools(cls, obj, allPools=True):
+        d = cls.ensure_driver(obj, "obj must be either an Entity or a Driver.")
+
+        pools = [Driver(a.entity) for a in d.parents()
+                 if isinstance(Driver(a.entity), ExclusivePool)]
+
+        if allPools:
+            for i in pools:
+                pools.extend(ExclusivePool.get_pools(i, allPools=True))
+
+        return pools
+
+
+class Pool(ExclusivePool):
+    """
+    A Pool is used to group Entities into a collection that shares attributes.
+
+    Pools
+    """
+
+    _driver_name = "pool"
+
+    def insert(self, thing):
+        """Insert the given Enity or Driver into this Entity.
+
+        Such that:
+
+        >>> A.insert(B)
+        >>> (B in A)
+        True
+
+        A given entity can only be in a Pool one time.
+        """
+
+        if thing in self:
+            raise PoolException("%s is already in pool %s." % (thing, self))
+
+        ExclusivePool.insert(self, thing)
+
+
+class UniquePool(ExclusivePool):
     _driver_name = "unique_pool"
 
     def insert(self, thing):
@@ -106,4 +110,4 @@ class UniquePool(Pool):
             raise PoolException("%s is already in UniquePool(s) %s." %
                                 (thing, pools))
 
-        Pool.insert(self, thing)
+        ExclusivePool.insert(self, thing)
