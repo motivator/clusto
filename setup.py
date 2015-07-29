1caa78630e5b204ad79b28d2b7106286adcc32d1
Jorge Gallegos <kad@blegh.net>
2015-07-29 15:24:49 -0700

    Bumping version in setup.py for release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ reqs = os.path.join(os.path.dirname(sys.argv[0]), 'requirements.txt')
 
 setuptools.setup(
     name = "clusto",
-    version = "0.8-dev",
+    version = "0.7.8",
     packages = setuptools.find_packages('src'),
     author = "Ron Gorodetzky",
     author_email = "ron@parktree.net",
